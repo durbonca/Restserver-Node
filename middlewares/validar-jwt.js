148ceccfcefec796d266755f46d6185ed1c8eb8e
durbonca <durbonca@gmail.com>
2023-05-12 12:24:23 -0400

    obtener informacion de usuario autenticado

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -11,8 +11,18 @@ const validarJWT = async (req, res, next) => {
     try {
         const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
         // leer el usuario que corresponde al uid
-        req.usuario = await Usuario.findById(uid);
-        req.uid = uid;
+        const usuario = await Usuario.findById(uid);
+
+        // Verificar si el usuario existe en la BD
+        if (!usuario) return res.status(401).json({
+            msg: 'Token no válido - usuario no existe en BD'
+        });
+
+        if (!usuario.estado) return res.status(401).json({
+            msg: 'Token no válido - usuario con estado: false'
+        });
+
+        req.usuario = usuario;
         next()
     } catch (error) {
         res.status(401).json({
