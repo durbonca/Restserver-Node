072422b74e655c23d9cbebd43a918b3cbb45364a
durbonca <durbonca@gmail.com>
2023-05-12 11:11:33 -0400

    login de usuario

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,9 +1,41 @@
-const loginPost = (req, res) => {
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
+
+const loginPost = async (req, res) => {
     const { correo, password } = req.body;
 
-    res.json({
-        msg: 'login ok', correo, password
+    // verificar si email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - correo'
     });
+
+    // usuario activo?
+    if (!usuario.estado) return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - estado: false'
+    });
+
+    // Password correcto
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - password'
+    });
+
+    // Generar JWT
+
+    try {
+        res.json({
+            msg: 'login ok', correo, password
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Error de servidor'
+        });
+
+    }
 };
 
 module.exports = {
